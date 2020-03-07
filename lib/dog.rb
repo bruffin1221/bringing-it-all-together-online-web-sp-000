5b83f28b5b849ea28f50857a542f11abc55e3773
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-07 17:21:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,6 @@ class Dog
 attr_accessor :name, :breed, :id
 
 def initialize(id:nil, name:, breed:)
-  #attributes.each {|key, value| self.send(("#{key}="), value)}
   @id=id
   @name=name
   @breed=breed
