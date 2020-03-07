14484f7cb5cdc9be8030eb01c50a8773e2c7d0b4
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-07 16:55:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,12 +68,11 @@ def self.find_or_create_by(name:, breed:)
 end
 
 def self.new_from_db(row)
-  dog_hash={}
   Dog.new(id, name, breed)
-  dog_hash[:dog.id]=row[0],
-  dog_hash[:dog.name]=row[1],
-  dog_hash[:dog.breed]=row[2]
-  dog_hash
+  dog.id=row[0]
+  dog.name=row[1]
+  dog.breed=row[2]
+  dog
 end
 
 def self.find_by_name(name)
